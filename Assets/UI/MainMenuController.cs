4056e3f4045965238bd9e800de62ea16287920ac
Ali Ba <aliba@nevada.unr.edu>
2023-11-04 15:11:46 -0700

    Updated Main Menu

--- Assets/UI/MainMenuController.cs
+++ Assets/UI/MainMenuController.cs
@@ -19,6 +19,11 @@ public class MainMenuController : MonoBehaviour
     [SerializeField]
     private Sprite _unmutedSprite;
     private bool _muted;
+    [Header("Levels")]
+    [SerializeField]
+    private VisualTreeAsset _levelsButtonsTemplate;
+    private VisualElement _levelsButtons;
+    private VisualElement _buttonsWrapper;
 
     private void Awake()
     {
@@ -26,6 +31,19 @@ public class MainMenuController : MonoBehaviour
 
         _playButton = _doc.rootVisualElement.Q<Button>("PlayButton");
         _playButton.clicked += PlayButtonOnClicked;
+
+        _levelsButtons = _levelsButtonsTemplate.CloneTree();
+        var level1Button = _levelsButtons.Q<Button>("Level1Button");
+        level1Button.clicked += Level1ButtonOnClicked;
+        var level2Button = _levelsButtons.Q<Button>("Level2Button");
+        level2Button.clicked += Level2ButtonOnClicked;
+        var level3Button = _levelsButtons.Q<Button>("Level3Button");
+        level3Button.clicked += Level3ButtonOnClicked;
+        var backButton = _levelsButtons.Q<Button>("BackButton");
+        backButton.clicked += BackButtonOnClicked;
+
+        //_levelsButtons = _doc.rootVisualElement.Q<Button>("SettingsButton");
+        _buttonsWrapper = _doc.rootVisualElement.Q<VisualElement>("Buttons");
 
         _settingsButton = _doc.rootVisualElement.Q<Button>("SettingsButton");
 
@@ -38,7 +56,24 @@ public class MainMenuController : MonoBehaviour
 
     private void PlayButtonOnClicked()
     {
+        _buttonsWrapper.Clear();
+        _buttonsWrapper.Add(_levelsButtons);
+        
+    }
+
+    private void Level1ButtonOnClicked()
+    {
         SceneManager.LoadScene("FirstLevel");
+    }
+
+    private void Level2ButtonOnClicked()
+    {
+        SceneManager.LoadScene("SecondLevel");
+    }
+
+    private void Level3ButtonOnClicked()
+    {
+        SceneManager.LoadScene("ThirdLevel");
     }
 
     private void ExitButtonOnClicked()
@@ -54,5 +89,13 @@ public class MainMenuController : MonoBehaviour
         _muteButton.style.backgroundImage = bg;
 
         AudioListener.volume = _muted ? 0 : 1;
+    }
+
+    private void BackButtonOnClicked()
+    {
+        _buttonsWrapper.Clear();
+        _buttonsWrapper.Add(_playButton);
+        _buttonsWrapper.Add(_settingsButton);
+        _buttonsWrapper.Add(_exitButton);
     }
 }
